7b48c74e24f7ae37eb185c7343702f25aff2bb90
qiongyu <linqiongyu@dingtalk.com>
2020-04-16 20:03:14 +0800

    my blog

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -78,12 +78,8 @@ module.exports = {
                 "text": "Tag"
             }
         },
-        "friendLink": [{
-                "title": "午后南杂",
-                "desc": "Enjoy when you can, and endure when you must.",
-                "email": "1156743527@qq.com",
-                "link": "https://www.recoluan.com"
-            },
+        "friendLink": [
+
             {
                 "title": "vuepress-theme-reco",
                 "desc": "A simple and beautiful vuepress Blog & Doc theme.",
