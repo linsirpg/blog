4626082f2b88d2f84a5ece778ed59540e1a73004
林琼宇 <5504714+lin_ruikang@user.noreply.gitee.com>
2020-06-03 16:08:06 +0800

    'mgBlog'

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -35,35 +35,11 @@ module.exports = {
                 "text": "Contact",
                 "icon": "reco-message",
                 "items": [{
-                        "text": "NPM",
-                        "link": "https://www.npmjs.com/~reco_luan",
-                        "icon": "reco-npm"
-                    },
-                    {
                         "text": "GitHub",
-                        "link": "https://github.com/recoluan",
-                        "icon": "reco-github"
+                        "link": "https://github.com/linsirpg",
+                        "icon": "logo.png"
                     },
-                    {
-                        "text": "简书",
-                        "link": "https://www.jianshu.com/u/cd674a19515e",
-                        "icon": "reco-jianshu"
-                    },
-                    {
-                        "text": "CSDN",
-                        "link": "https://blog.csdn.net/recoluan",
-                        "icon": "reco-csdn"
-                    },
-                    {
-                        "text": "博客圆",
-                        "link": "https://www.cnblogs.com/luanhewei/",
-                        "icon": "reco-bokeyuan"
-                    },
-                    {
-                        "text": "WeChat",
-                        "link": "https://mp.weixin.qq.com/s/mXFqeUTegdvPliXknAAG_A",
-                        "icon": "reco-wechat"
-                    }
+
                 ]
             }
         ],
